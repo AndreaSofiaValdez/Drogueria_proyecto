0f08ea68c39604bf450def1cdb35db436740e85b
Andrea Valdez <andreavaldez7c@gmail.com>
2024-05-27 20:45:20 -0600

    aumento de tamaño fuente boton login

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -163,7 +163,7 @@ namespace Drogueria_proyecto
             this.button1.BackColor = System.Drawing.Color.SlateGray;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(290, 441);
             this.button1.Margin = new System.Windows.Forms.Padding(1);
